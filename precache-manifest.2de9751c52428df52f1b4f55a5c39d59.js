cf240e40668b7496a261387da7604bf111c1a316
System Administrator <root@Akhils-MacBook-Air.local>
2019-07-13 21:23:47 +0530

    Updates

---
+++ precache-manifest.2de9751c52428df52f1b4f55a5c39d59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5af7052462dc0a23c8b026596dcc964",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "f339040d12120657d5a0",
+    "url": "/ToDoApp/static/css/main.60b622e8.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/ToDoApp/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "f339040d12120657d5a0",
+    "url": "/ToDoApp/static/js/main.49862ebc.chunk.js"
+  },
+  {
+    "revision": "fb1067834985d33aa2a1",
+    "url": "/ToDoApp/static/js/runtime~main.f2c45213.js"
+  }
+]);
